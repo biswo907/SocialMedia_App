50b4652d3125e975a46f5877d7a2fd699c721820
Biswo <biswajitdash907@gmail.com>
2024-08-25 21:48:40 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
